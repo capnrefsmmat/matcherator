139e51f9b3fb91eb1e9119deb7d60b6d7ab0a70f
Alex Reinhart <alex@refsmmat.com>
2026-02-13 12:21:41 -0500

    Restructure rules to include examples, base tests on these
    Sorry, Gemini, but this is a better way to do testing.

--- match_rules.py
+++ match_rules.py
@@ -2,9 +2,53 @@
 
 import argparse
 import json
+from collections import namedtuple
 
 import spacy
-from spacy.matcher import Matcher, DependencyMatcher
+from spacy.matcher import Matcher, DependencyMatcher, PhraseMatcher
+
+Matchers = namedtuple("Matchers", ["nlp", "plain", "dependency", "phrase"])
+
+def initialize_matchers(rules, model):
+    """Initialize matchers from a rule specification.
+
+    The rule spec is read from the JSON rule file. We then initialize spaCy
+    matchers and load the rules. Returns the three matchers (Matcher,
+    DependencyMatcher, and PhraseMatcher) in a namedtuple.
+    """
+
+    nlp = spacy.load(model)
+    matcher = Matcher(nlp.vocab, validate=True)
+    dep_matcher = DependencyMatcher(nlp.vocab, validate=True)
+    phrase_matcher = PhraseMatcher(nlp.vocab, attr="LOWER",
+                                   validate=True)
+
+    for rulename, rule in rules["Matcher"].items():
+        matcher.add(rulename, rule["rules"])
+
+    for rulename, rule in rules["DependencyMatcher"].items():
+        dep_matcher.add(rulename, rule["rules"])
+
+    for rulename, rule in rules["PhraseMatcher"].items():
+        phrase_matcher.add(rulename, list(nlp.tokenizer.pipe(rule["rules"])))
+
+    return Matchers(nlp, matcher, dep_matcher, phrase_matcher)
+
+
+def match_text(matchers, text):
+    """Apply the matchers to a text string.
+
+    Returns a dictionary of Matcher, DependencyMatcher, and PhraseMatcher matches.
+    """
+
+    doc = matchers.nlp(text)
+
+    return {
+        "Matcher": matchers.plain(doc, as_spans=True),
+        "DependencyMatcher": matchers.dependency(doc),
+        "PhraseMatcher": matchers.phrase(doc, as_spans=True)
+    }
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Match a text against a JSON ruleset.")
@@ -17,23 +61,18 @@ if __name__ == "__main__":
     ruleset = open(args.ruleset, "r")
     rules = json.load(ruleset)
 
-    nlp = spacy.load(args.model)
-    matcher = Matcher(nlp.vocab, validate=True)
-    dep_matcher = DependencyMatcher(nlp.vocab, validate=True)
+    matchers = initialize_matchers(rules, args.model)
+    matches = match_text(matchers, args.text)
 
-    for rulename, rule in rules["Matcher"].items():
-        matcher.add(rulename, rule)
-
-    for rulename, rule in rules["DependencyMatcher"].items():
-        dep_matcher.add(rulename, rule)
-
-    doc = nlp(args.text)
-
-    matches = matcher(doc, as_spans=True)
-    for m in matches:
+    print("Matcher")
+    for m in matches["Matcher"]:
         print(f"{m.label_}: {m.text}")
 
-    dep_matches = dep_matcher(doc)
-    for match_id, token_ids in dep_matches:
+    print("DependencyMatcher")
+    for match_id, token_ids in matches["DependencyMatcher"]:
         print(nlp.vocab.strings[match_id],
               ", ".join([doc[tok].text for tok in token_ids]))
+
+    print("PhraseMatcher")
+    for m in matches["PhraseMatcher"]:
+        print(f"{m.label_}: {m.text}")
---
+++ tests/test_ruleset.py
@@ -0,0 +1,51 @@
+"""Test rule files to ensure the examples and counterexamples match correctly."""
+
+from .. import match_rules
+
+import json
+import pytest
+import os.path
+
+RULE_FILES = ["pseudobiber.json"]
+MODEL = "en_core_web_sm"
+
+def check_rule_in_spans(rulename, matches):
+    for m in matches:
+        if m.label_ == rulename:
+            return True
+
+    return False
+
+def check_rule_in_matches(rulename, matches, nlp):
+    for match_id, _ in matches:
+        if nlp.vocab.strings[match_id] == rulename:
+            return True
+
+    return False
+
+def test_examples():
+    for rule_file in RULE_FILES:
+        rules = json.load(open(os.path.join("../rules/", rule_file), "r"))
+
+        matchers = match_rules.initialize_matchers(rules, MODEL)
+
+        for rulename, rule in rules["Matcher"].items():
+            for example in rule.get("examples", []):
+                matches = match_rules.match_text(matchers, example)
+
+                assert check_rule_in_spans(rulename, matches["Matcher"]), \
+                    f"Matcher: Expected rule `{rulename}` to match `{example}`"
+
+        for rulename, rule in rules["DependencyMatcher"].items():
+            for example in rule.get("examples", []):
+                matches = match_rules.match_text(matchers, example)
+
+                assert check_rule_in_matches(rulename, matches["DependencyMatcher"], matchers.nlp), \
+                    f"DependencyMatcher: Expected rule `{rulename}` to match `{example}`"
+
+        for rulename, rule in rules["PhraseMatcher"].items():
+            for example in rule.get("examples", []):
+                matches = match_rules.match_text(matchers, example)
+
+                assert check_rule_in_spans(rulename, matches["PhraseMatcher"]), \
+                    f"PhraseMatcher: Expected rule `{rulename}` to match `{example}`"
