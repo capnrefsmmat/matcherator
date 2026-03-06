1102d489d9cc45161543c51af4b429343178ed6c
Alex Reinhart <alex@refsmmat.com>
2026-03-05 19:45:49 -0500

    Use subtests so we can continue testing past one failure
    Allows all rules to be tested even if one fails (though the first
    failing example will still terminate testing of a rule).

--- tests/test_ruleset.py
+++ tests/test_ruleset.py
@@ -24,47 +24,50 @@ def check_rule_in_matches(rulename, matches, nlp):
     return False
 
 
-def test_examples():
+def test_examples(subtests):
     for rule_file in RULE_FILES:
         rules = json.load(open(os.path.join("./rules/", rule_file), "r"))
 
         matchers = match_rules.initialize_matchers(rules, MODEL)
 
         for rulename, rule in rules["Matcher"].items():
-            for example in rule.get("examples", []):
-                matches = match_rules.match_text(matchers, example)
+            with subtests.test(f"Matcher: {rulename}"):
+                for example in rule.get("examples", []):
+                    matches = match_rules.match_text(matchers, example)
 
-                assert check_rule_in_spans(rulename, matches["Matcher"]), \
-                    f"Matcher: Expected rule `{rulename}` to match `{example}`"
+                    assert check_rule_in_spans(rulename, matches["Matcher"]), \
+                        f"Expected rule `{rulename}` to match `{example}`"
 
-            for example in rule.get("counterexamples", []):
-                matches = match_rules.match_text(matchers, example)
+                for example in rule.get("counterexamples", []):
+                    matches = match_rules.match_text(matchers, example)
 
-                assert not check_rule_in_spans(rulename, matches["Matcher"]), \
-                    f"Matcher: Expected rule `{rulename}` to not match `{example}`"
+                    assert not check_rule_in_spans(rulename, matches["Matcher"]), \
+                        f"Expected rule `{rulename}` to not match `{example}`"
 
         for rulename, rule in rules["DependencyMatcher"].items():
-            for example in rule.get("examples", []):
-                matches = match_rules.match_text(matchers, example)
+            with subtests.test(f"DependencyMatcher: {rulename}"):
+                for example in rule.get("examples", []):
+                    matches = match_rules.match_text(matchers, example)
 
-                assert check_rule_in_matches(rulename, matches["DependencyMatcher"], matchers.nlp), \
-                    f"DependencyMatcher: Expected rule `{rulename}` to match `{example}`"
+                    assert check_rule_in_matches(rulename, matches["DependencyMatcher"], matchers.nlp), \
+                        f"Expected rule `{rulename}` to match `{example}`"
 
-            for example in rule.get("counterexamples", []):
-                matches = match_rules.match_text(matchers, example)
+                for example in rule.get("counterexamples", []):
+                    matches = match_rules.match_text(matchers, example)
 
-                assert not check_rule_in_matches(rulename, matches["DependencyMatcher"], matchers.nlp), \
-                    f"DependencyMatcher: Expected rule `{rulename}` to not match `{example}`"
+                    assert not check_rule_in_matches(rulename, matches["DependencyMatcher"], matchers.nlp), \
+                        f"Expected rule `{rulename}` to not match `{example}`"
 
         for rulename, rule in rules["PhraseMatcher"].items():
-            for example in rule.get("examples", []):
-                matches = match_rules.match_text(matchers, example)
+            with subtests.test(f"PhraseMatcher: {rulename}"):
+                for example in rule.get("examples", []):
+                    matches = match_rules.match_text(matchers, example)
 
-                assert check_rule_in_spans(rulename, matches["PhraseMatcher"]), \
-                    f"PhraseMatcher: Expected rule `{rulename}` to match `{example}`"
+                    assert check_rule_in_spans(rulename, matches["PhraseMatcher"]), \
+                        f"Expected rule `{rulename}` to match `{example}`"
 
-            for example in rule.get("counterexamples", []):
-                matches = match_rules.match_text(matchers, example)
+                for example in rule.get("counterexamples", []):
+                    matches = match_rules.match_text(matchers, example)
 
-                assert not check_rule_in_spans(rulename, matches["PhraseMatcher"]), \
-                    f"PhraseMatcher: Expected rule `{rulename}` to not match `{example}`"
+                    assert not check_rule_in_spans(rulename, matches["PhraseMatcher"]), \
+                        f"Expected rule `{rulename}` to not match `{example}`"
