9c628ca32ade707a4f1f7ed711fcbfb12148c2b5
Alex Reinhart <alex@refsmmat.com>
2026-06-13 12:06:25 -0400

    Completely re-architect as spaCy pipeline
    Register a matcherator extension on spaCy Doc object and use it to track
    matches, allowing us to integrate with spaCy's native pipelines, which
    can be parallelized.

--- match_rules.py
+++ match_rules.py
@@ -5,20 +5,99 @@ import json
 from collections import defaultdict, namedtuple
 
 import spacy
+from spacy.language import Language
+from spacy.tokens import Doc, Span
 from spacy.matcher import Matcher, DependencyMatcher, PhraseMatcher
 from spacymoji import Emoji
 
 import pandas as pd
 
-Matchers = namedtuple("Matchers",
-                      ["nlp", "plain", "dependency", "phrase", "feature_names"])
+Doc.set_extension("matcherator", default=None)
 
-def initialize_matchers(rules, model):
-    """Initialize matchers from a rule specification.
+@Language.factory("matcherator", default_config={"path": None, "normalize": False})
+def matcherator(nlp, name, path, normalize):
+    return Matcherator(nlp, path, normalize)
 
-    The rule spec is read from the JSON rule file. We then initialize spaCy
-    matchers and load the rules. Returns the three matchers (Matcher,
-    DependencyMatcher, and PhraseMatcher) in a namedtuple.
+class Matcherator:
+    def __init__(self, nlp, path, normalize):
+        # Set up matchers
+        self.matcher = Matcher(nlp.vocab, validate=True)
+        self.dep_matcher = DependencyMatcher(nlp.vocab, validate=True)
+        self.phrase_matcher = PhraseMatcher(nlp.vocab, attr="LOWER",
+                                            validate=True)
+
+        # Load rules
+        if path is None:
+            raise ValueError("No path to rules file provided")
+
+        with open(path, "r") as ruleset:
+            rules = json.load(ruleset)
+
+        matcher_features = {k for k in rules["Matcher"].keys()}
+        for rulename, rule in rules["Matcher"].items():
+            self.matcher.add(rulename, rule["rules"], greedy="FIRST")
+
+        dep_features = {k for k in rules["DependencyMatcher"].keys()}
+        for rulename, rule in rules["DependencyMatcher"].items():
+            self.dep_matcher.add(rulename, rule["rules"])
+
+        phrase_features = {k for k in rules["PhraseMatcher"].keys()}
+        for rulename, rule in rules["PhraseMatcher"].items():
+            self.phrase_matcher.add(rulename, list(nlp.tokenizer.pipe(rule["rules"])))
+
+        self.features = matcher_features | dep_features | phrase_features
+        self.nlp = nlp
+
+    def __call__(self, doc):
+        matches = self.matcher(doc)
+        dep_matches = self.dep_matcher(doc)
+        phrase_matches = self.phrase_matcher(doc)
+
+        doc._.matcherator = defaultdict(list)
+
+        # note that everything we add to doc._ must be serializable if we're
+        # going to use this in a parallel pipeline, since multiprocessing has to
+        # serialize the data back to the main process. Span objects are not, so
+        # we have to stick to token ranges or indices.
+        for match_id, start, end in matches:
+            doc._.matcherator[self.nlp.vocab.strings[match_id]].append((start, end))
+
+        for match_id, token_ids in dep_matches:
+            doc._.matcherator[self.nlp.vocab.strings[match_id]].append(token_ids)
+
+        for match_id, start, end in phrase_matches:
+            doc._.matcherator[self.nlp.vocab.strings[match_id]].append((start, end))
+
+        return doc
+
+
+def count_matches(doc, normalize=False):
+    """Return dictionary of match counts for a Document.
+
+    For a Document that has already been matched, iterate through the matches
+    and produce a dictionary of counts of each match type. If normalize is True,
+    normalize to rates per 1,000 tokens.
+
+    """
+
+    if normalize:
+        return {name: len(value) / len(doc) * 1000
+                for name, value
+                in doc._.matcherator.items()}
+    else:
+        return {name: len(value)
+                for name, value
+                in doc._.matcherator.items()}
+
+
+def count_matches_texts(model, rule_path, doc_ids, texts, normalize=False):
+    """Apply spaCy and the given rules to a set of texts and count matches.
+
+    Returns a Pandas data frame with one row per document and one column per
+    feature, whose entries give the count of matches of each feature in each
+    document. If normalize is True, counts are normalized to rates per 1,000
+    tokens.
+
     """
 
     # no need for named-entity recognition
@@ -27,134 +106,34 @@ def initialize_matchers(rules, model):
     # Add emoji detection pipeline
     nlp.add_pipe("emoji", first=True)
 
-    matcher = Matcher(nlp.vocab, validate=True)
-    dep_matcher = DependencyMatcher(nlp.vocab, validate=True)
-    phrase_matcher = PhraseMatcher(nlp.vocab, attr="LOWER",
-                                   validate=True)
+    # Add matcherator pipeline
+    nlp.add_pipe("matcherator", config={"path": rule_path})
 
-    matcher_features = {k for k in rules["Matcher"].keys()}
-    for rulename, rule in rules["Matcher"].items():
-        matcher.add(rulename, rule["rules"], greedy="FIRST")
-
-    dep_features = {k for k in rules["DependencyMatcher"].keys()}
-    for rulename, rule in rules["DependencyMatcher"].items():
-        dep_matcher.add(rulename, rule["rules"])
-
-    phrase_features = {k for k in rules["PhraseMatcher"].keys()}
-    for rulename, rule in rules["PhraseMatcher"].items():
-        phrase_matcher.add(rulename, list(nlp.tokenizer.pipe(rule["rules"])))
-
-    return Matchers(nlp, matcher, dep_matcher, phrase_matcher,
-                    matcher_features | dep_features | phrase_features)
-
-
-def match_text(matchers, text):
-    """Apply the matchers to a text string.
-
-    Returns a dictionary of Matcher, DependencyMatcher, and PhraseMatcher
-    matches.
-
-    """
-
-    doc = matchers.nlp(text)
-
-    return {
-        "Document": doc,
-        "Matcher": matchers.plain(doc, as_spans=True),
-        "DependencyMatcher": matchers.dependency(doc),
-        "PhraseMatcher": matchers.phrase(doc, as_spans=True)
-    }
-
-def count_matches(matchers, matches, normalize=False):
-    """Count the matches of each feature.
-
-    Takes the output of match_text() and produces a dictionary of {feature_name:
-    count} pairs. If normalize is True, count is normalized to a rate per 1,000
-    tokens.
-
-    """
-
-    matches_out = defaultdict(lambda: 0)
-
-    for m in matches["Matcher"]:
-        matches_out[m.label_] += 1
-
-    for match_id, token_id in matches["DependencyMatcher"]:
-        matches_out[matchers.nlp.vocab.strings[match_id]] += 1
-
-    for m in matches["PhraseMatcher"]:
-        matches_out[m.label_] += 1
-
-    if normalize:
-        num_tokens = len(matches["Document"])
-
-        matches_out = {
-            key: value / num_tokens * 1000
-            for key, value in matches_out.items()
-        }
-
-    return matches_out
-
-def count_matches_texts(matchers, doc_ids, texts, normalize=False):
-    """Apply matchers to a sequence of documents.
-
-    Returns a Pandas data frame. Each row is one document; each column is one
-    matched feature. Entries are counts of each feature. If normalize is True,
-    count is normalized to a rate per 1,000 tokens.
-
-    """
-
-    assert len(doc_ids) == len(texts), \
-        "Number of doc_ids doesn't match number of texts"
-
-    match_counts = [count_matches(matchers, match_text(matchers, text),
-                                  normalize)
-                    for text in texts
-                    if text is not None]
-
-    # handle cases where text is None
+    # Eliminate Nones in texts
     doc_ids = [doc_ids[ii]
                for ii in range(len(doc_ids))
                if texts[ii] is not None]
+    texts = [t for t in texts if t is not None]
 
-    out = pd.DataFrame.from_records(match_counts, index=doc_ids).fillna(0)
+    match_counts = [count_matches(doc, normalize)
+                    for doc in nlp.pipe(texts, n_process=-1)]
 
-    # If we didn't match a feature, add a column of 0s instead of omitting it
-    # from the data frame
-    missing_cols = matchers.feature_names - set(out.columns)
+    return pd.DataFrame.from_records(match_counts, index=doc_ids).fillna(0)
 
-    for col in missing_cols:
-        out[col] = 0
 
-    return out
+def print_matches(nlp, doc):
+    def stringify_match(m):
+        if isinstance(m, tuple):
+            # span of (begin, end) tokens
+            begin, end = m
+            return doc[begin:end].text
 
-def print_matches(matchers, matches):
-    def print_dict(d):
-        for label, items in d.items():
-            out = "; ".join(items)
-            print(f"{label}: {out}")
+        # must be a list of token IDs
+        return ", ".join(doc[t].text for t in m)
 
-    print("Matcher")
-    matcher_matches = defaultdict(list)
-    for m in matches["Matcher"]:
-        matcher_matches[m.label_].append(m.text)
-
-    print_dict(matcher_matches)
-
-    print("DependencyMatcher")
-    dep_matches = defaultdict(list)
-    for match_id, token_ids in matches["DependencyMatcher"]:
-        match_tokens = ", ".join(matches["Document"][t].text for t in token_ids)
-        dep_matches[matchers.nlp.vocab.strings[match_id]].append(match_tokens)
-
-    print_dict(dep_matches)
-
-    print("PhraseMatcher")
-    phrase_matches = defaultdict(list)
-    for m in matches["PhraseMatcher"]:
-        phrase_matches[m.label_].append(m.text)
-
-    print_dict(phrase_matches)
+    for feature, matches in doc._.matcherator.items():
+        out = "; ".join([stringify_match(m) for m in matches])
+        print(f"{feature}: {out}")
 
 
 if __name__ == "__main__":
@@ -165,10 +144,15 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    ruleset = open(args.ruleset, "r")
-    rules = json.load(ruleset)
+    # no need for named-entity recognition
+    nlp = spacy.load(args.model, disable=["ner"])
 
-    matchers = initialize_matchers(rules, args.model)
-    matches = match_text(matchers, args.text)
+    # Add emoji detection pipeline
+    nlp.add_pipe("emoji", first=True)
 
-    print_matches(matchers, matches)
+    # Add matcherator pipeline
+    nlp.add_pipe("matcherator", config={"path": args.ruleset})
+
+    doc = nlp(args.text)
+
+    print_matches(nlp, doc)
--- tests/test_ruleset.py
+++ tests/test_ruleset.py
@@ -2,11 +2,14 @@
 
 from .. import match_rules
 
+import itertools
 import json
 import pytest
 import os.path
 
-RULE_FILES = ["pseudobiber.json"]
+import spacy
+
+RULE_FILES = ["pseudobiber.json", "llm-patterns.json"]
 MODEL = "en_core_web_sm"
 
 def check_rule_in_spans(rulename, matches):
@@ -26,48 +29,30 @@ def check_rule_in_matches(rulename, matches, nlp):
 
 def test_examples(subtests):
     for rule_file in RULE_FILES:
-        rules = json.load(open(os.path.join("./rules/", rule_file), "r"))
+        rule_path = os.path.join("./rules/", rule_file)
 
-        matchers = match_rules.initialize_matchers(rules, MODEL)
+        rules = json.load(open(rule_path, "r"))
 
-        for rulename, rule in rules["Matcher"].items():
-            with subtests.test(f"Matcher: {rulename}"):
+        nlp = spacy.load(MODEL, disable=["ner"])
+
+        # Add emoji detection pipeline
+        nlp.add_pipe("emoji", first=True)
+
+        # Add matcherator pipeline
+        nlp.add_pipe("matcherator", config={"path": rule_path})
+
+        for rulename, rule in itertools.chain(rules["Matcher"].items(),
+                                              rules["DependencyMatcher"].items(),
+                                              rules["PhraseMatcher"].items()):
+            with subtests.test(rulename):
                 for example in rule.get("examples", []):
-                    matches = match_rules.match_text(matchers, example)
+                    doc = nlp(example)
 
-                    assert check_rule_in_spans(rulename, matches["Matcher"]), \
-                        f"Expected rule `{rulename}` to match `{example}`"
+                    assert len(doc._.matcherator[rulename]) > 0, \
+                        f"Expected `{rulename}` to match `{example}`"
 
                 for example in rule.get("counterexamples", []):
-                    matches = match_rules.match_text(matchers, example)
+                    doc = nlp(example)
 
-                    assert not check_rule_in_spans(rulename, matches["Matcher"]), \
-                        f"Expected rule `{rulename}` to not match `{example}`"
-
-        for rulename, rule in rules["DependencyMatcher"].items():
-            with subtests.test(f"DependencyMatcher: {rulename}"):
-                for example in rule.get("examples", []):
-                    matches = match_rules.match_text(matchers, example)
-
-                    assert check_rule_in_matches(rulename, matches["DependencyMatcher"], matchers.nlp), \
-                        f"Expected rule `{rulename}` to match `{example}`"
-
-                for example in rule.get("counterexamples", []):
-                    matches = match_rules.match_text(matchers, example)
-
-                    assert not check_rule_in_matches(rulename, matches["DependencyMatcher"], matchers.nlp), \
-                        f"Expected rule `{rulename}` to not match `{example}`"
-
-        for rulename, rule in rules["PhraseMatcher"].items():
-            with subtests.test(f"PhraseMatcher: {rulename}"):
-                for example in rule.get("examples", []):
-                    matches = match_rules.match_text(matchers, example)
-
-                    assert check_rule_in_spans(rulename, matches["PhraseMatcher"]), \
-                        f"Expected rule `{rulename}` to match `{example}`"
-
-                for example in rule.get("counterexamples", []):
-                    matches = match_rules.match_text(matchers, example)
-
-                    assert not check_rule_in_spans(rulename, matches["PhraseMatcher"]), \
-                        f"Expected rule `{rulename}` to not match `{example}`"
+                    assert len(doc._.matcherator[rulename]) == 0, \
+                        f"Expected `{rulename}` to not match `{example}`"
