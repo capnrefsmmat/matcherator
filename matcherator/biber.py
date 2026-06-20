56d1f8e28e0af3b4c319b659ef13244cb3ed947f
Alex Reinhart <alex@refsmmat.com>
2026-06-20 19:40:00 -0400

    Handle a f_60_that_deletion edge case

--- matcherator/biber.py
+++ matcherator/biber.py
@@ -44,19 +44,29 @@ class BiberMatcherator(Matcherator):
 
         return matches
 
-    def _filter_that_deletion(self, matches):
+    def _filter_that_deletion(self, doc, matches):
         """Produce f_60_that_deletion.
 
         We match subordinator clauses with or without "that", and separately
-        match those with "that". That deletion is the clauses without.
+        match those with "that". "That" deletion is the clauses without.
+
+        We also match subordinator clauses where it introduces a wh-clause
+        instead. For example, "The rationale helps ensure that workers
+        understand what they should do." matches our DependencyMatcher rules.
+        Here "understand" it introduces a wh-question ("what they should do")
+        and not a clause like "that it is bad."
 
         """
 
+
         subord_thats = {(m[0], m[1], m[2])
                         for m in matches["f_60_that_deletion:that"]}
+        wh_clauses = {(m[0], m[1], m[2])
+                      for m in matches["f_60_that_deletion:wh_clause"]}
 
         that_deleted = [m for m in matches["f_60_that_deletion:all"]
-                        if (m[0], m[1], m[2]) not in subord_thats]
+                        if (m[0], m[1], m[2]) not in subord_thats
+                        and (m[0], m[1], m[2]) not in wh_clauses]
 
         matches["f_60_that_deletion"] = that_deleted
 
@@ -66,7 +76,7 @@ class BiberMatcherator(Matcherator):
         matches = self._match(doc)
 
         matches = self._filter_passives(matches)
-        matches = self._filter_that_deletion(matches)
+        matches = self._filter_that_deletion(doc, matches)
 
         doc._.matcherator_biber = matches
         doc._.matcherator_biber_features = self.features
